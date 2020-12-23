04d631be9844e07d3b8e9a70a69ab799fee3a8a7
Nataliya Sayenko <nataliyasayenko@gmail.com>
2020-12-22 23:49:47 -0500

    fix

--- day3/solution.js
+++ day3/solution.js
@@ -13,9 +13,10 @@ const inputArray = fs
 // Part A
 const checkTrees = (right, down) => {
   let hit = 0;
-  for (let traversed = 0; traversed < inputArray.length; traversed += down) {
+  for (let traversed = 0; traversed * down < inputArray.length; traversed++) {
     const column = (traversed * right) % inputArray[traversed].length;
-    if (inputArray[traversed][column] === "#") {
+    const x = traversed * down;
+    if (inputArray[x][column] === "#") {
       hit++;
     }
   }
@@ -25,3 +26,18 @@ const checkTrees = (right, down) => {
 };
 
 checkTrees(3, 1); //187
+
+// part B
+
+const partB = () => {
+  const answer =
+    checkTrees(3, 1) *
+    checkTrees(1, 1) *
+    checkTrees(5, 1) *
+    checkTrees(7, 1) *
+    checkTrees(1, 2);
+
+  return answer;
+};
+
+console.log(partB());
